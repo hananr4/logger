05f5389397197b0706930aad091cb5dd9760ccca
Hanan Rodriguez <hanan.rodriguez@greensoft.com.ec>
2023-08-21 16:23:14 -0500

    A Consola

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+public class Program
+{
+  static void Main()
+  {
+    using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+    ILogger logger = loggerFactory.CreateLogger<Program>();
+    logger.LogInformation("Mensaje informativo {0}", Directory.GetCurrentDirectory());
+    logger.LogWarning("Casi ocurrio un error");
+    logger.LogError("Aqui ocurrio un error");
+  }
+}
